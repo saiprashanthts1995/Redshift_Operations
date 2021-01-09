7c31399069170f1d4f8f0b9b559e14a8279fa152
Sai Prashanth T S <saiprashanthts@gmail.com>
2021-01-10 05:17:31 +0530

    updated

--- scripts/load_fwf_file.py
+++ scripts/load_fwf_file.py
@@ -5,31 +5,89 @@ __purpose__ = 'Load fixed width file into redshift cluster'
 import pandas as pd
 from sqlalchemy import create_engine
 from sqlalchemy import text
+from utils import read_col_specs, udf_log, read_config
 
 
 class Load_Fixed_Width:
-    def __init__(self, file_name):
+    def __init__(self, file_name, log):
         """
         Constructor to accept the name of the file
         :param file_name:
         """
         self.file_name = file_name
+        self.log = log
 
-    def read_fixed_width(self):
+    def read_fixed_width(self, spec_path):
+        """
+        This function is used to read the fixed width file
+        :param spec_path:
+        :return:
+        """
+        # to get specification details of the fixed width file
+        spec_details = read_col_specs(spec_path, self.log)
+
+        # read the fixed width file by providing the arguments in the widths as list of position
         data = pd.read_fwf(self.file_name,
-                           colspecs={})
+                           widths=spec_details[1])
 
-    def create_db_connection(self):
-        pass
+        # changing the column names of dataframe
+        data.columns = spec_details[0]
+        self.log.info('Data frame after reading first 5 rows is shown below {}'.format(data.head()))
+        return data
 
-    def load_into_table(self):
-        pass
+    def create_db_connection(self, config_path):
+        """
+        This is used to create db connection
+        :param config_path:
+        :return: Connection
+        """
+        config = read_config(config_path, self.log)
+        config = config.get('Redshift_Dev')
+        engine = create_engine("postgres://{user}:{password}@{host}:{port}/{dbname}".format(**config))
+        connection = engine.connect()
+        self.log.info('Connection to Redshift has got successfully connected')
+        return connection
 
-    def main(self):
-        pass
+    def load_into_table(self, data, connection, table_name, schema):
+        """
+        Loading data into Postgres table
+        :param data:
+        :param connection:
+        :param table_name:
+        :param schema:
+        :return:
+        """
+        data.to_sql(name=table_name,
+                    schema=schema,
+                    con=connection,
+                    if_exists='replace')
+        self.log.info('Data loaded to table {} completed successfully'.format(table_name))
+        return "Success"
+
+    def main(self, spec_path, config_path, table_name, schema):
+        """
+        This is the main function to call subsequent class functions
+        :param spec_path:
+        :param config_path:
+        :param table_name:
+        :param schema:
+        :return:
+        """
+        self.log.info('Process of loading file to Redshift started successfully')
+        data = self.read_fixed_width(spec_path)
+        connection = self.create_db_connection(config_path)
+        self.load_into_table(data, connection, table_name, schema)
 
 
 if __name__ == '__main__':
-    fwf = Load_Fixed_Width('../data/departments_fixed_width_file.txt')
-    print(fwf.main())
+    log = udf_log('default.log')
+    log.info('Welcome! This is useful for loading file into redshift')
+    fwf = Load_Fixed_Width('../data/departments_fixed_width_file.txt', log)
+    print(fwf.main('col_specs_fwf_departments.json', 'config.json', 'departments', 'practice'))
+    # import logging
+    # logging.basicConfig(filename='dummy',
+    #                     level=logging.INFO,
+    #                     format())
+    # logging.info('Hi')
+    # logging.warning('Hello')
 
--- scripts/utils.py
+++ scripts/utils.py
@@ -3,20 +3,42 @@ __email__ = 'saiprashanthts@gmail.com'
 __purpose__ = 'To create user defined functions which can be leveraged in the other modules'
 
 import json
-import loguru
+from loguru import logger
 from datetime import datetime
 import sys
+import pandas as pd
+import yaml
 
 
-def read_col_specs(col_spec_file_name):
+def read_table_details(table_detail_path, log, section):
+    """
+    This UDF is used to read the table details of the section which we wish to load
+    :param table_detail_path:
+    :param log:
+    :param section:
+    :return:
+    """
+    with open('../config/tbl_details.yaml') as f:
+        tbl_details = yaml.load(f)
+    log.info('tbl_details.yaml read completed. Details loaded for section {}'.format(section))
+    print(tbl_details)
+    return tbl_details['table_details'][section]
+
+
+def read_col_specs(section, log):
     """
     This is used to read config of col specification of fixed width file
     :param col_spec_file_name:
+    :param log:
     :return:
     """
     with open("../config/{}".format(col_spec_file_name)) as file:
         col_spec_content = json.load(file)
-    return col_spec_content
+    col_df = pd.DataFrame(data=col_spec_content.items(), columns=['name', 'widths'])
+    col_name = col_df['name'].values.tolist()
+    widths = col_df['widths'].values.tolist()
+    log.info('Column Specification for Fixed width is shown below {}'.format(col_spec_content))
+    return [col_name, widths]
 
 
 def timeit(main_method):
@@ -30,6 +52,7 @@ def timeit(main_method):
         result = main_method(*args, **kwargs)
         end_time = datetime.now()
         print("Total time taken is {}".format(end_time - start_time))
+        log.info('Total time taken is {}'.format(end_time - start_time))
         return result
     return wrapper
 
@@ -46,28 +69,42 @@ def udf_exception(main_method):
             return result
         except Exception as e:
             print(e)
+            log.exception("Following exception occurred {}".format(e))
+            log.warning("Please correct and re-run the process")
             sys.exit(1)
     return wrapper
 
 
-def read_config(config_file_name):
+def read_config(config_file_name, log, section):
     """
     This UDF is used to read the config details of redshift
     :param config_file_name:
+    :param log:
     :return:
     """
     config_path = open("../config/{}".format(config_file_name))
     config = json.load(config_path)
+    log.info("config.json read successfully")
     config_path.close()
     return config
 
 
-def udf_log(log_path):
-    pass
+def udf_log(log_name):
+    """
+    This is an user defined function which is used to log all activities
+    :param log_name:
+    :return:
+    """
+    logger.add(f"../logs/{log_name}",
+               retention="10 days",
+               rotation="10 days",
+               level="INFO")
+    return logger
 
 
 if __name__ == "__main__":
-    print("hello")
-    print(read_col_specs('col_specs_fwf_departments.json'))
-    print(read_config("config.json"))
-
+    log = udf_log('default.log')
+    log.info('Welcome! This is useful for loading file into redshift')
+    print(read_table_details('../config/tbl_details.yaml', log, section='departments'))
+    # print(read_col_specs('col_specs_fwf_departments.json', log))
+    # print(read_config("config.json", log))
